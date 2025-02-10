4df61c8f73161f01fdda6ea35b906d25e55f319e
jetal patel <145288109+j2patel4545@users.noreply.github.com>
2025-02-10 12:11:08 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from '../style'
 import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-        <div className='flex-1 flex flex-col justify-start mr-10'>
+      <div className={`${styles.flexStart} md:flex-row  flex-col mb-8 w-full`}>
+        <div className=' px-10 flex-1 flex flex-col justify-start mr-10'>
           <img
             src={logo}
             alt='logo'
@@ -17,19 +18,21 @@ const Footer = () => {
             A new way to make the payments easy, reliable and secure.         
           </p>
         </div>
-        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+        <div className=' px-10 flex-[1.5] w-full flex flex-row gap-20 flex-wrap md:mt-0 mt-10'>
           {footerLinks.map((link) => (
-            <div key={link.title} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
-              <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
+            <div key={link.title} className='flex flex-col  ss:my-0 my-4 min-w-[150px]'>
+              <h4 className='font-poppins font-medium text-[18px]  ding-[27px] text-white'>
                 {link.title}
               </h4>
-              <ul className='list-none mt-4'>
+              <ul className='list-none mt-4'> 
                 {link.links.map((item, index) => (
-                  <li 
+                 <li className='pt-3'>
+                   <Link  to={item.link}
                     key={item.name} 
                     className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== link.links.length - 1 ? 'mb-4' : 'mb-0'}`}
                   >
                     {item.name}
+                  </Link> 
                   </li>
                 ))}
               </ul>
@@ -37,7 +40,9 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
+    
+    
+      <div className=' px-10 w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
         <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
           2025. All Rights Reserved by krinergy Digital
         </p>  
