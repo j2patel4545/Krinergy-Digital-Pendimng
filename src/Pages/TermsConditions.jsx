72a478bcff03b039f3b561146b57eab547b3f69e
jetal patel <145288109+j2patel4545@users.noreply.github.com>
2025-02-12 18:30:01 +0530

    Add files via upload

---
+++ src/Pages/TermsConditions.jsx
@@ -0,0 +1,34 @@
+const TermsConditions = () => {
+    return (
+        <div className="container text-white pt-32 px-20 mx-auto p-6">
+            <h1 className="text-2xl font-bold mb-4">Terms & Conditions of Krinergy Digital</h1>
+            
+            <section className="mb-4">
+                <h2 className="text-xl font-semibold">1. Services Provided</h2>
+                <p>DigiFarm Technologies provides services such as software development, IT consulting, network management, and cybersecurity solutions. The specific services to be provided will be outlined in the contract or project scope agreed upon between the company and the client.</p>
+            </section>
+
+            <section className="mb-4">
+                <h2 className="text-xl font-semibold">2. Client Responsibilities</h2>
+                <p>The client agrees to provide all necessary information and cooperation to facilitate the timely and efficient delivery of services. The client also agrees to pay for the services rendered as per the payment terms outlined in the project agreement.</p>
+            </section>
+
+            <section className="mb-4">
+                <h2 className="text-xl font-semibold">3. Payment Terms</h2>
+                <p>Payments will be made according to the pricing structure agreed upon by both parties, either as a one-time fee or through a recurring subscription model. Invoices will be issued on a [weekly/monthly/quarterly] basis and are due within 30 days from the invoice date.</p>
+            </section>
+
+            <section className="mb-4">
+                <h2 className="text-xl font-semibold">4. Confidentiality</h2>
+                <p>Both parties agree to maintain confidentiality regarding any proprietary or confidential information shared during the course of the agreement. This obligation remains in effect even after the termination of the services.</p>
+            </section>
+
+            <section className="mb-4">
+                <h2 className="text-xl font-semibold">5. Data Privacy and Security</h2>
+                <p>The Company agrees to handle the Client’s data with care and in accordance with applicable data protection laws. The Client agrees to comply with all relevant laws concerning data privacy when using the Company’s services.</p>
+            </section>
+        </div>
+    );
+};
+
+export default TermsConditions;
