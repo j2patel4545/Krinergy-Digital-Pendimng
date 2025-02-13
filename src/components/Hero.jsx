38d3cb66101a873ba71fe7dad7b200225f7965f7
jetal patel <145288109+j2patel4545@users.noreply.github.com>
2025-02-13 11:21:40 +0530

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -55,14 +55,14 @@ const Hero = () => {
       </div>
       
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <motion.img 
+        {/* <motion.img 
           src={robot} alt='billing' 
           className='w-[100%] h-[100%] relative z-[5]'
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1 }}
           whileHover={{ scale: 1.05 }}
-        />
+        /> */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"/>
